067798206bc25fa8b92dc21a019bea1d87f59a29
Robert Keller <robert_keller44@icloud.com>
2020-01-04 03:31:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -38,7 +38,7 @@ def total_gross(source)
   total = 0
   index = 0
   while index < source.length do
-  puts directors_totals(source)[index]
+  puts directors_totals(source)list_of_directors(source)[index]
     index += 1
   end
   total
